0c61c9310c838b87e6939b38fd37e2144e996506
Nelson Fleig <nelson.fleig@rover.com>
2024-05-23 00:05:01 +0200

    separate scripts

---
+++ run_after_lunar_vim.sh
@@ -0,0 +1,36 @@
+# Lunarvim install functions
+install_lunarvim()
+{
+  if [ ! -d "$HOME/.local/share/lunarvim/lvim" ]; then
+    yes | LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
+  fi
+}
+
+maintain_lunarvim_plugins()
+{
+  lvim=$HOME/.local/bin/lvim
+  $lvim --headless "+Lazy! sync" +qa
+
+  # Plugins
+  $lvim --headless "+Lazy! sync" +qa
+
+  # Parsers
+  # This is done automatically, but this way it will be done before LunarVim is opened
+  # Mostly relevant in Codespaces as it will speed up the initial experience
+  $lvim --headless "+TSUpdateSync comment" +qa
+  $lvim --headless "+TSUpdateSync regex" +qa
+  $lvim --headless "+TSUpdateSync markdown_inline" +qa
+  $lvim --headless "+TSUpdateSync python" +qa
+  $lvim --headless "+TSUpdateSync tsx" +qa
+  $lvim --headless "+TSUpdateSync javascript" +qa
+  $lvim --headless "+TSUpdateSync typescript" +qa
+  $lvim --headless "+TSUpdateSync json" +qa
+  $lvim --headless "+TSUpdateSync bash" +qa
+}
+
+run_on_linux_only() {
+  install_lunarvim
+  maintain_lunarvim_plugins
+}
+
+run_on_linux_only
--- run_once_after_fzf_install.sh
+++ run_once_after_fzf_install.sh
@@ -1,3 +1,8 @@
 if [[ "$(uname)" == "Linux" ]]; then
-  yes | ~/.fzf/install --no-bash --no-fis
+  # fzf (required by zoxide)
+  if [ -d "$HOME/.fzf" ]; then
+    rm -rf $HOME/.fzf
+  fi
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install --all
 fi
