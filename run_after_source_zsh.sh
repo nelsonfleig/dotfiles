7b29d9bbc9cefd01c36178846641aa93264ef5aa
Nelson Fleig <nelson.fleig@rover.com>
2024-05-28 11:56:13 +0200

    source bash aliases after .zshrc

--- run_after_source_zsh.sh
+++ run_after_source_zsh.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
 
+source ~/.zshrc
+
 if [ -n "${CODESPACES}" ]; then
   git config --global user.email "nelson.fleig@rover.com"
-  [ -f ~/.bash_aliases ] && source ~/.bash_aliases
+  ([ -f ~/.bash_aliases ] && cd .. && source ~/.bash_aliases)
 fi
 
-source ~/.zshrc
+
