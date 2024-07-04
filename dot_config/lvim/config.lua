6c21918f0cc4b16717f89d38c01f80965866db2b
Nelson Fleig <nelson.fleig@rover.com>
2024-07-04 13:18:24 +0200

    add ruby and update secondary JAVA_HOME

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -8,8 +8,9 @@ lvim.builtin.treesitter.ensure_installed = {
   "python"
 }
 
-local formatters = require("lvim.lsp.null-ls.formatters")
-formatters.setup { { name = "black" } }
+-- TODO: Check if it is enough to just use flake8
+-- local formatters = require("lvim.lsp.null-ls.formatters")
+-- formatters.setup { { name = "black" } }
 
 local linters = require("lvim.lsp.null-ls.linters")
 linters.setup { { command = "flake8", args = { "--ignore=E203" }, filetypes = { "python" } } }
