5114c18084da0168dcbe223792c5693a0534f7ec
Nelson Fleig <nfleig@hotmail.com>
2024-10-27 17:33:14 +0100

    simplify vhyrro plugin setup

--- dot_config/nvim-vhyrro/lua/plugins.lua
+++ dot_config/nvim-vhyrro/lua/plugins.lua
@@ -123,15 +123,21 @@ require("lazy").setup({
             local lspconfig = require("mason-lspconfig")
             lspconfig.setup({
                 ensure_installed = { "lua_ls", "clangd" },
+                handlers = {
+                    function (server_name) -- default handler (optional)
+                        require("lspconfig")[server_name].setup {}
+                    end,
+                }
             })
-            lspconfig.setup_handlers {
-                -- The first entry (without a key) will be the default handler
-                -- and will be called for each installed server that doesn't have
-                -- a dedicated handler.
-                function (server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {}
-                end,
-            }
+            -- NOTE: This can also be done in the setup function (see above)
+            -- lspconfig.setup_handlers {
+            --     -- The first entry (without a key) will be the default handler
+            --     -- and will be called for each installed server that doesn't have
+            --     -- a dedicated handler.
+            --     function (server_name) -- default handler (optional)
+            --         require("lspconfig")[server_name].setup {}
+            --     end,
+            -- }
         end
     },
 })
