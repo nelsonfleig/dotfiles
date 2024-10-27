b59a3a7ed840828328d9f05a5d725708a45f3289
Nelson Fleig <nfleig@hotmail.com>
2024-10-27 22:52:37 +0100

    remove unused nvim configs and change tmux default-terminal

--- dot_config/nvim-starter-kit/README.md
+++
@@ -1,7 +0,0 @@
-# Nerd Signals - Neovim Starter Kit
-
-
-Main starter from https://youtu.be/33NLeHvFKxU
-
-Python extensions from  https://www.youtube.com/watch?v=jWZ_JeLgDxU
-
--- dot_config/nvim-starter-kit/lua/plugins/hardtime-nvim.lua
+++
@@ -1,6 +0,0 @@
--- WARNING - This plugin can drive you crazy!
-return {
-   -- "m4xshen/hardtime.nvim",
-   -- dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-   -- opts = {}
-}
