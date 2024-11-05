76ce77a7b59b86e30b3f4556acb03dfca2b56f64
Nelson Fleig <nelson.fleig@rover.com>
2024-11-06 00:02:28 +0100

    pre install tmux plugins in codespace

--- run_after_source_zsh.sh
+++ run_after_source_zsh.sh
@@ -7,8 +7,12 @@ if [ -n "${CODESPACES}" ]; then
   # change default shell to zsh
   sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
 
-  # tmux version in Codespace require's the config file to be in $HOME
+  # install tmux plugins
+  ~/.config/tmux/plugins/tpm/bin/install_plugins
+  
+# tmux version in Codespace require's the config file to be in $HOME
   cp ~/.config/tmux/tmux.conf ~/.tmux.conf
+
 fi
 
 source ~/.zshrc
