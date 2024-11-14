3dade6816034499ec5b478f99d75ee687d61f9e4
Nelson Fleig <nfleig@hotmail.com>
2024-11-15 00:06:38 +0100

    update lualine config

--- dot_config/nvim/lua/nelson/plugins/lualine.lua
+++ dot_config/nvim/lua/nelson/plugins/lualine.lua
@@ -23,6 +23,19 @@ return {
       sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch' },
+        lualine_c = {
+          {
+            'diagnostics',
+            symbols = {
+              error = ' ',
+              warn = ' ',
+              info = ' ',
+              hint = ' ',
+            },
+          },
+          { 'filetype', icon_only = true, separator = '', padding = { left = 1, right = 0 } },
+          'filename',
+        },
         lualine_x = {
           {
             lazy_status.updates,
@@ -30,8 +43,16 @@ return {
             color = { fg = '#ff9e64' },
           },
           { 'encoding' },
-          { 'fileformat' },
-          { 'filetype' },
+          -- { 'fileformat' },
+        },
+        lualine_y = {
+          { 'progress', separator = ' ', padding = { left = 1, right = 0 } },
+          { 'location', padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {
+          function()
+            return ' ' .. os.date '%R'
+          end,
         },
       },
       extensions = { 'neo-tree', 'lazy' },
