1e78220eb90ffc15594ccdd0d8f77239cf059b76
Nelson Fleig <nfleig@hotmail.com>
2024-11-16 02:10:34 +0100

    add initial basic rover neovim config

---
+++ dot_config/nvim-rover/lua/rover/config/init.lua
@@ -0,0 +1,15 @@
+-- [[ Global Settings ]]
+-- Set <space> as the leader key
+-- ch NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- Set default colorscheme
+vim.cmd.colorscheme("habamax")
+
+require("rover.config.options")
+require("rover.config.keymaps")
+require("rover.config.lazy")
---
+++ dot_config/nvim-rover/lua/rover/plugins/telescope.lua
@@ -0,0 +1,35 @@
+return {
+  { -- Fuzzy Finder (files, lsp, etc)
+    'nvim-telescope/telescope.nvim',
+    event = 'VimEnter',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { -- If encountering errors, see telescope-fzf-native README for installation instructions
+        'nvim-telescope/telescope-fzf-native.nvim',
+
+        -- `build` is used to run some command when the plugin is installed/updated.
+        -- This is only run then, not every time Neovim starts up.
+        build = 'make',
+
+        -- `cond` is a condition used to determine whether this plugin should be
+        -- installed and loaded.
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+    },
+    config = function()
+      require('telescope').setup({})
+
+      -- Enable Telescope extensions if they are installed
+      pcall(require('telescope').load_extension, 'fzf')
+
+      -- See `:help telescope.builtin`
+      local builtin = require 'telescope.builtin'
+      vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Find Files (Root Dir)' })
+      vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Grep (Root Dir)' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Buffers' })
+    end,
+  },
+}
