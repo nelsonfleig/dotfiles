b7654807e7103062534f30324a169183c3376ed4
Nelson Fleig <nfleig@hotmail.com>
2024-11-16 12:54:14 +0100

    add global Snacks in lazydev for nvim-nelson

--- dot_config/nvim-nelson/lua/nelson/plugins/telescope.lua
+++ dot_config/nvim-nelson/lua/nelson/plugins/telescope.lua
@@ -26,7 +26,6 @@ return {
         end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
-
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
