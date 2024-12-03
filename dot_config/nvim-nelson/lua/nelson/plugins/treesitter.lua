2cc3547c524bd8c2082227cba06ed38f8c8bdaa6
Nelson Fleig <nelson.fleig@rover.com>
2024-12-03 22:01:58 +0100

    add textobjects keymaps for parameters and swaps

--- dot_config/nvim-nelson/lua/nelson/plugins/treesitter.lua
+++ dot_config/nvim-nelson/lua/nelson/plugins/treesitter.lua
@@ -53,6 +53,9 @@ return {
           -- Automatically jump forward to textobj, similar to targets.vim
           lookahead = true,
           keymaps = {
+            ['aa'] = { query = '@parameter.outer', desc = 'Select outer part of a parameter/argument' },
+            ['ia'] = { query = '@parameter.inner', desc = 'Select inner part of a parameter/argument' },
+
             ['al'] = { query = '@loop.outer', desc = 'Select outer part of a loop' },
             ['il'] = { query = '@loop.inner', desc = 'Select inner part of a loop' },
 
@@ -66,20 +69,19 @@ return {
             ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class' },
           },
         },
-        -- TODO: Disable these in which-key
-        -- swap = {
-        --   enable = true,
-        --   swap_next = {
-        --     ['<leader>na'] = '@parameter.inner', -- swap parameters/argument with next
-        --     ['<leader>n:'] = '@property.outer', -- swap object property with next
-        --     ['<leader>nm'] = '@function.outer', -- swap function with next
-        --   },
-        --   swap_previous = {
-        --     ['<leader>pa'] = '@parameter.inner', -- swap parameters/argument with prev
-        --     ['<leader>p:'] = '@property.outer', -- swap object property with prev
-        --     ['<leader>pm'] = '@function.outer', -- swap function with previous
-        --   },
-        -- },
+        swap = {
+          enable = true,
+          swap_next = {
+            ['<leader>na'] = { query = '@parameter.inner', desc = 'which_key_ignore' }, -- swap parameters/argument with next
+            ['<leader>n:'] = { '@property.outer', desc = 'which_key_ignore' }, -- swap object property with next
+            ['<leader>nm'] = { '@function.outer', desc = 'which_key_ignore' }, -- swap function with next
+          },
+          swap_previous = {
+            ['<leader>pa'] = { '@parameter.inner', desc = 'which_key_ignore' }, -- swap parameters/argument with prev
+            ['<leader>p:'] = { '@property.outer', desc = 'which_key_ignore' }, -- swap object property with prev
+            ['<leader>pm'] = { '@function.outer', desc = 'which_key_ignore' }, -- swap function with previous
+          },
+        },
         move = {
           enable = true,
           set_jumps = true, -- whether to set jumps in the jumplist
