7339e1305f0df0ad41d68f59529ee7e3b8500901
Nelson Fleig <nelson.fleig@rover.com>
2025-10-22 10:57:04 +0200

    remove ruff linting and use lsp instead

--- dot_config/nvim-nelson/lua/nelson/plugins/linting.lua
+++ dot_config/nvim-nelson/lua/nelson/plugins/linting.lua
@@ -5,12 +5,13 @@ return {
     local lint = require 'lint'
 
     -- NOTE: Linting for JS/TS is provided by `eslint` lsp server
+    -- NOTE: Linting for python is provided by `ruff` lsp server on save
     lint.linters_by_ft = {
       -- javascript = { 'eslint_d' },
       -- typescript = { 'eslint_d' },
       -- javascriptreact = { 'eslint_d' },
       -- typescriptreact = { 'eslint_d' },
-      python = { 'flake8' },
+      -- python = { 'ruff' },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
