cde4d67c9f1f17fcd3f2ebe8bd568f09b502100d
Nelson Fleig <nelson.fleig@rover.com>
2025-11-13 12:08:46 +0100

    replace formatting and linting with ruff

--- dot_config/nvim-nelson/lua/nelson/plugins/formatting.lua
+++ dot_config/nvim-nelson/lua/nelson/plugins/formatting.lua
@@ -46,8 +46,9 @@ return { -- Autoformat
       yaml = { 'eslint_d', 'prettierd' },
       lua = { 'stylua' },
       -- Conform can also run multiple formatters sequentially
-      python = { 'isort', 'black' },
+      -- python = { 'isort', 'black' },
       -- python = { 'isort', 'ruff' }, -- will be needed once formatting with ruff is enabled in codespaces
+      python = { 'ruff' }, -- will be needed once formatting with ruff is enabled in codespaces
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
