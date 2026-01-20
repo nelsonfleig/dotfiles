13b3a73dfce858e8828c38edb258548df738121c
Nelson Fleig <nelson.fleig@rover.com>
2026-01-20 10:51:38 +0100

    change ruff formatter to ruff_format

--- dot_config/nvim-nelson/lua/nelson/plugins/formatting.lua
+++ dot_config/nvim-nelson/lua/nelson/plugins/formatting.lua
@@ -48,7 +48,7 @@ return { -- Autoformat
       -- Conform can also run multiple formatters sequentially
       -- python = { 'isort', 'black' },
       -- python = { 'isort', 'ruff' }, -- will be needed once formatting with ruff is enabled in codespaces
-      python = { 'ruff' }, -- will be needed once formatting with ruff is enabled in codespaces
+      python = { 'ruff_format' },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
